88922924a0bb7dcb3781777fda6f37da68b4cc63
Ankit Kumar <158794632+Oyeankit6@users.noreply.github.com>
2025-04-30 15:14:32 +0530

    Update App.js

--- Server/App.js
+++ Server/App.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "https://learningmangemnt.netlify.app/",
+    origin: "https://lerningwith-ankit.netlify.app/",
     credentials: true,
   })
 );
